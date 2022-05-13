c99e858a0896dcbbcafc98814b36ac3443428291
Robin Joseph <rjrdutg@gmail.com>
2022-05-13 09:09:27 +0200

    try_to_fill function tried but have to been fixed
    Co-authored-by: Salim <SalimKad@users.noreply.github.com>

--- main.py
+++ main.py
@@ -3,11 +3,39 @@ gave_list = [50, 11, 13, 7, 15, 18, 9, 20, 12, 10, 8, 16, 14, 17, 19, 11, 13, 7,
 def main(list,max):
     final_list = []
     sum = 0
-    j = 0
 
-    if try_to_fill(list, max)
+    gave_list = list_sort(list)
+    final_list = try_to_fill(gave_list, max, final_list)
+    print(final_list)
+    
+    while (exact_sum(final_list, sum) == -1):
+        print(final_list)
+        gave_list.remove(final_list[-1])
+        final_list.remove(final_list[-1])
+        final_list = try_to_fill(gave_list, max, final_list)
+        print(final_list)
 
-def try_to_fill(list, sum):
+        
+
+def try_to_fill(gave_list, max, started_list):
+    #started list is the list that is already filled
+    final_list = started_list
+    used_list = gave_list
+
+    while exact_sum(used_list,max) < 0 and len(used_list) > 0:
+        if exact_sum(used_list,max) + used_list[0] <= 0 :
+            final_list.append(used_list[0])
+            used_list.remove(used_list[0])
+        else :
+            used_list.remove(used_list[0])
+
+    return final_list
+            
+        
+
+
+
+"""def try_to_fill(list, sum):
     final_list = []
 
     for i in range(0, len(list)):
@@ -18,7 +46,7 @@ def try_to_fill(list, sum):
     if (exact_sum(final_list, sum) == 0):
         return final_list
     else : 
-        return final_list[-1]
+        return final_list[-1]"""
 
 
 def list_sort(list):
@@ -41,7 +69,7 @@ def exact_sum(list, sum):
     list_sum = 0
     for i in list:
         list_sum += i
-    if list_sum < sum:
+    if list_sum < sum or list==[]:
         return -1
     elif list_sum == sum:
         return 0
@@ -50,3 +78,4 @@ def exact_sum(list, sum):
 
 
 max_weight = int(input("Enter the maximum weight: "))
+main(list_sort(gave_list), max_weight)
